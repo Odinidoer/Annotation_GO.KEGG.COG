8245316298383f8a1f3c8b0cce7112df552c8703
Odinidoer <1031091157@qq.com>
2017-09-05 15:23:39 +0800

    change another way to deal except

--- script/download_all.py
+++ script/download_all.py
@@ -79,7 +79,8 @@ organism2ko = {dir}/organism/
 [COG]
 
 [DOCX]
-docx_dir = {dir}/docx_dir/
+raw_docx_dir = /mnt/ilustre/users/jun.yan/scripts/pm_report_module/20170410/
+new_docx_dir = /mnt/ilustre/users/jun.yan/scripts/pm_report_module/{time}
 '''.format(time = now_time,dir = new_dir))
 
 ##获取新的配置文件信息
@@ -109,7 +110,7 @@ os_mkdir(pathwayshtml)
 pathwaysimage = cfg.get("KEGG","pathwaysimage")
 os_mkdir(pathwaysimage)
 
-if u'快速下载KEGG' == u'快速下载KEGG':
+if u'快速下载KEGG' == u'快速下载KEGG1':
 	with open('%s/tmp_del_later.list' %pathwayshtml,'w')as tmp_w:
 		tmp_w.write('\n'.join(pathway_list))
 		
@@ -123,6 +124,7 @@ if u'快速下载KEGG' == u'快速下载KEGG':
 				png.write(requests.get(png_url,timeout=123).content)	
 			print(rank,ko)
 		except:
+			print('error',rank,ko)
 			FIN.write(ko+'\n')
 			FIN.flush()
 			
@@ -139,6 +141,31 @@ if u'快速下载KEGG' == u'快速下载KEGG':
 			
 	os.remove('%s/tmp_del_later.list' %pathwayshtml)	
 	
+if u'快速下载KEGG' == u'快速下载KEGG':
+	download_ko_list = pathway_list
+	def download_png_html(ko):
+		ko = str(ko)
+		try:
+			kgml_url = 'http://www.kegg.jp/kegg-bin/show_pathway?ko%s' %ko	
+			with open('%s/ko%s.kgml' %(pathwayshtml,ko),'w')as kgml:
+				kgml.write(requests.get(kgml_url,timeout=123).text)	
+			png_url = 'http://rest.kegg.jp/get/ko%s/image' %ko
+			with open('%s/ko%s.png' %(pathwaysimage,ko),'wb')as png:
+				png.write(requests.get(png_url,timeout=123).content)
+		except:
+			print('error',ko)
+			return ko
+			
+	while len(download_ko_list)>0:
+		print('download_ko_list remain %s' %len(download_ko_list))
+		new_download_ko_list = []
+		p = Pool(4)
+		new_download_ko_list.append(p.map(download_png_html,download_ko_list))
+		p.close()
+		p.join()
+		download_ko_list = [x for x in new_download_ko_list if x]
+		print(download_ko_list)
+	
 print('\nall pathway`s png and html downloaded correctly')			
 			
 ##获取所有物种信息
@@ -149,7 +176,7 @@ all_organism_data = requests.get(organism_list_url).text
 organism_list = [x.split('\t')[1] for x in all_organism_data.split('\n') if '\t' in x]
 download_organism_list = organism_list	
 
-if u'想快速下载物种' == u'想快速下载物种':
+if u'想快速下载物种' == u'想快速下载物种1':
 	with open('%s/tmp_del_later.list' %organism2ko,'w')as tmp_w:
 		tmp_w.write('\n'.join(map(str,organism_list)))
 		
@@ -176,7 +203,28 @@ if u'想快速下载物种' == u'想快速下载物种':
 			p.join()
 			
 	os.remove('%s/tmp_del_later.list' %organism2ko)		
-			
+	
+if u'想快速下载物种' == u'想快速下载物种':
+	def get_organism_ko_list(organism):
+		organism = str(organism)
+		try:			
+			organism_ko_url = 'http://rest.kegg.jp/list/pathway/%s' %organism
+			with open('%s/%s.txt' %(organism2ko,organism),'w')as org_file:
+				org_file.write(requests.get(organism_ko_url).text)
+		except:			
+			print('error:',organism)
+			return organism
+	
+	while len(download_organism_list)>0:
+		print('download_organism_list remain %s' %len(download_organism_list))
+		new_download_organism_list = []
+		p = Pool(4)
+		new_download_organism_list.append(p.map(get_organism_ko_list,download_organism_list))
+		p.close()
+		p.join()
+		download_organism_list = [x for x in new_download_organism_list if x]
+		print(download_organism_list)
+		
 print('\nall siginal org`s pathway.list downloaded correctly')			
 		
 ##处理得到物种信息
@@ -237,17 +285,15 @@ print('\n before docx')
 with open(go_obo_name,'r')as go_obo_r:
 	'''format-version: 1.2
 	data-version: releases/2017-07-28'''
-	format_version = go_obo_r.readline()
-	data_version = go_obo_r.readline()
+	format_version = go_obo_r.readline().strip()
+	data_version = go_obo_r.readline().strip()
 	
-print(u'''此次项目中，公司使用的GO数据更新日期是：%s,对应GO官方网站的数据格式是：%s
-,对应GO官方网站的数据更新日期是：%s。''' %(now_time,format_version,data_version))
+print(u'此次项目中，公司使用的GO数据更新日期是：%s,对应GO官方网站的数据格式是：%s,对应GO官方网站的数据更新日期是：%s。' %(now_time,format_version,data_version))
 
 kegg_release_url = 'http://www.kegg.jp/kegg/docs/relnote.html'
 kegg_release_data = requests.get(kegg_release_url).text
 kegg_release = re.findall(r'<h4>Current release</h4>\n\n(.*?)\n<ul>',kegg_release_data)
-print(u'''此次项目中，公司使用的KEGG数据更新日期是：%s,对应KEGG官方网站的数据格式是：%s'''
-%(now_time,kegg_release))
+print(u'此次项目中，公司使用的KEGG数据更新日期是：%s,对应KEGG官方网站的数据格式是：%s'%(now_time,kegg_release))
 
 ##链接config.ini
 cmd = 'rm config.ini && ln -s %s/config.ini config.ini' %(new_dir)
